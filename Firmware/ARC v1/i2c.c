200fd0f4de3aa95886f44de092667bf94f864a33
Dinesh Parimi <dinesh.parimi@live.com>
2017-09-27 15:46:32 -0700

    i2c init, start, stop

--- Firmware/ARC v1/i2c.c
+++ Firmware/ARC v1/i2c.c
@@ -1,6 +1,26 @@
-const int i2c_addr = 0x6B;
+// This code borrows from http://www.embedds.com/programming-avr-i2c-interface/
+// Access control: 
+
+#define F_CPU 16000000
+// 115200 baud
+#define BITRATE 62 // ( clock / baud - 16) / 2
+#define I2C_ADDR = 0x6B;
 
 int main() {
-	
+	TWSR = 0;
+	TWBR = BITRATE;
 }
 
+void i2c_init() {
+	TWSR = 0;
+	TWBR = BITRATE;
+}
+
+void i2c_start() {
+	TWCR = (1 << TWEN) | (1 << TWSTA) | (1 << TWINT);
+	while !(TWCR & (1 << TWINT));
+}
+
+void i2c_stop() {
+	TWCR = (1 << TWEN) | (1 << TWSTO) | (1 << TWINT);
+}
