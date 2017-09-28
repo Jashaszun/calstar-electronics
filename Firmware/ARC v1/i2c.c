4d969cdc479d428b678ade8ff7ea233f02f27542
Dinesh Parimi <dinesh.parimi@live.com>
2017-09-27 17:05:19 -0700

    more work on i2c

--- Firmware/ARC v1/i2c.c
+++ Firmware/ARC v1/i2c.c
@@ -3,8 +3,12 @@
 
 #define F_CPU 16000000
 // 115200 baud
-#define BITRATE 62 // ( clock / baud - 16) / 2
-#define I2C_ADDR = 0x6B;
+#define BITRATE 62 // ( clock / baud - 16) / (2 * prescaler)
+#define I2C_ADDR 0x6B
+#define I2C_READ 1
+#define I2C_WRITE 0
+#define X_ACCEL_MSB 0x28
+#define X_ACCEL_LSB 0x29
 
 int main() {
 	TWSR = 0;
@@ -12,15 +16,41 @@ int main() {
 }
 
 void i2c_init() {
-	TWSR = 0;
-	TWBR = BITRATE;
+	TWSR = 0; // prescaler = 1
+	TWBR = BITRATE; // load into bitrate reg
 }
 
 void i2c_start() {
-	TWCR = (1 << TWEN) | (1 << TWSTA) | (1 << TWINT);
-	while !(TWCR & (1 << TWINT));
+	TWCR = (1 << TWEN) | (1 << TWSTA) | (1 << TWINT); // transmit start condition
+	while !(TWCR & (1 << TWINT)); // wait for interrupt flag
+	if (TWSR != 0x08) { // get status, should be 0x08
+		error();
+	}
 }
 
 void i2c_stop() {
-	TWCR = (1 << TWEN) | (1 << TWSTO) | (1 << TWINT);
+	TWCR = (1 << TWEN) | (1 << TWSTO) | (1 << TWINT); // transmit stop condition
+}
+
+void i2c_write(char byte) {
+	TWDR = byte;
+	TWCR = (1 << TWINT) | (1 << TWEN); // clear interrupt flag to transmit
+	while !(TWCR & (1 << TWINT));
+	if (TWSR == 0x38) {
+		error();
+	}
+}
+
+short xAccel() {
+	short x;
+	i2c_start();
+	i2c_write((I2C_ADDR << 1) + I2C_WRITE); // slave address
+	i2c_write(X_ACCEL_MSB << 1) // register address
+	i2c_start(); // repeated start
+	i2c_write((I2C_ADDR << 1) + I2C_READ);
+	return x;
+}
+
+void error() {
+	// do something with the outputs here
 }
