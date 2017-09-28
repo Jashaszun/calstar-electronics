62a6745c5d11b8fc42984c7d0285b6d4915ca73a
Dinesh Parimi <dinesh.parimi@live.com>
2017-09-28 11:20:36 -0700

    fixed compilation issue and added macros for getting different values

--- Firmware/ARC v1/i2c.c
+++ Firmware/ARC v1/i2c.c
@@ -1,4 +1,4 @@
-// This code borrows from http://www.embedds.com/programming-avr-i2c-interface/
+// This code borrows from http://www.embedds.com/programming-avr-I2C-interface/
 // Access control: 
 
 #include <avr/io.h>
@@ -11,39 +11,37 @@
 #define I2C_WRITE 0
 #define X_ACCEL_MSB 0x28
 #define X_ACCEL_LSB 0x29
+#define Y_ACCEL_MSB 0x2A
+#define Y_ACCEL_LSB 0x2B
+#define Z_ACCEL_MSB 0x2C
+#define Z_ACCEL_LSB 0x2D
 
-int main() {
-	DDRD = 0xFF;
-	DDRB = 0xFF;
-	PORTD = 0x00;
-	PORTB = 0x00;
-	i2c_init();
-	int x = xAccel();
-	PORTB = 0xFF;
-	PORTD = 0xFF;
+void error() {
+	// PORTB = 0xFF;
+	// PORTD = 0xFF;
 }
 
-void i2c_init() {
+void I2C_init() {
 	TWSR = 0; // prescaler = 1
 	TWBR = BITRATE; // load into bitrate reg
 }
 
-void i2c_start() {
+void I2C_start() {
 	TWCR = (1 << TWEN) | (1 << TWSTA) | (1 << TWINT); // transmit start condition
-	while !(TWCR & (1 << TWINT)); // wait for interrupt flag
+	while (!(TWCR & (1 << TWINT))); // wait for interrupt flag
 	if (TWSR != 0x08) { // get status, should be 0x08
 		error();
 	}
 }
 
-void i2c_stop() {
+void I2C_stop() {
 	TWCR = (1 << TWEN) | (1 << TWSTO) | (1 << TWINT); // transmit stop condition
 }
 
-void i2c_write(char byte) {
+void I2C_write(char byte) {
 	TWDR = byte;
 	TWCR = (1 << TWINT) | (1 << TWEN); // clear interrupt flag to transmit
-	while !(TWCR & (1 << TWINT));
+	while (!(TWCR & (1 << TWINT)));
 	if ((TWSR == 0x38) || (TWSR == 0x48)) {
 		error();
 	} else { // ACK received
@@ -51,32 +49,70 @@ void i2c_write(char byte) {
 	}
 }
 
-char i2c_read_nack() {
+char I2C_read_nack() {
 	TWCR = (1 << TWINT) | (1 << TWEN);
-	while !(TWCR & (1 << TWINT));
+	while (!(TWCR & (1 << TWINT)));
 	return TWDR;
 }
 
-short xAccel() {
+/*short xAccel() {
 	short x;
-	i2c_start();
-	i2c_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
-	i2c_write(X_ACCEL_MSB << 1) // register address
-	i2c_start(); // repeated start
-	i2c_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
-	x = (i2c_read_nack() << 8);
-	i2c_stop();
-	i2c_start();
-	i2c_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
-	i2c_write(X_ACCEL_LSB << 1) // register address
-	i2c_start(); // repeated start
-	i2c_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
-	x |= i2c_read_nack();
-	i2c_stop();
+	I2C_start();
+	I2C_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
+	I2C_write(X_ACCEL_MSB << 1) // register address
+	I2C_start(); // repeated start
+	I2C_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
+	x = (I2C_read_nack() << 8);
+	I2C_stop();
+	I2C_start();
+	I2C_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
+	I2C_write(X_ACCEL_LSB << 1) // register address
+	I2C_start(); // repeated start
+	I2C_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
+	x |= I2C_read_nack();
+	I2C_stop();
 	return x;
+}*/
+
+short lsm9ds1_read(char addr1, char addr2) {
+	short rv;
+	I2C_start();
+	I2C_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
+	I2C_write(addr1 << 1); // register address
+	I2C_start(); // repeated start
+	I2C_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
+	rv = (I2C_read_nack() << 8);
+	I2C_stop();
+	I2C_start();
+	I2C_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
+	I2C_write(addr2 << 1); // register address
+	I2C_start(); // repeated start
+	I2C_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
+	rv |= I2C_read_nack();
+	I2C_stop();
+	return rv;
 }
 
-void error() {
-	// PORTB = 0xFF;
-	// PORTD = 0xFF;
+short lsm9ds1_xAccel() {
+	return lsm9ds1_read(X_ACCEL_MSB, X_ACCEL_LSB);
 }
+
+short lsm9ds1_yAccel() {
+	return lsm9ds1_read(Y_ACCEL_MSB, Y_ACCEL_LSB);
+}
+
+short lsm9ds1_zAccel() {
+	return lsm9ds1_read(Z_ACCEL_MSB, Z_ACCEL_LSB);
+}
+
+int main() {
+	DDRD = 0xFF;
+	DDRB = 0xFF;
+	PORTD = 0x00;
+	PORTB = 0x00;
+	I2C_init();
+	int x = lsm9ds1_xAccel();
+	PORTB = 0xFF;
+	PORTD = 0xFF;
+}
+
