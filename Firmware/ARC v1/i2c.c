0b30eb7a5c94c8b7f648b1a5821059a4cb52ae65
Dinesh Parimi <dinesh.parimi@live.com>
2017-09-27 17:23:27 -0700

    working i2c probably

--- Firmware/ARC v1/i2c.c
+++ Firmware/ARC v1/i2c.c
@@ -36,10 +36,17 @@ void i2c_write(char byte) {
 	TWDR = byte;
 	TWCR = (1 << TWINT) | (1 << TWEN); // clear interrupt flag to transmit
 	while !(TWCR & (1 << TWINT));
-	if (TWSR == 0x38) {
+	if ((TWSR == 0x38) || (TWSR == 0x48)) {
 		error();
+	} else { // ACK received
+		TWCR |= (1 << TWINT); // clear ACK
 	}
-	TWCR |= (1 << TWINT); // clear SAK
+}
+
+char i2c_read_nack() {
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while !(TWCR & (1 << TWINT));
+	return TWDR;
 }
 
 short xAccel() {
@@ -49,6 +56,15 @@ short xAccel() {
 	i2c_write(X_ACCEL_MSB << 1) // register address
 	i2c_start(); // repeated start
 	i2c_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
+	x = (i2c_read_nack() << 8);
+	i2c_stop();
+	i2c_start();
+	i2c_write((I2C_ADDR << 1) + I2C_WRITE); // slave address, write mode
+	i2c_write(X_ACCEL_LSB << 1) // register address
+	i2c_start(); // repeated start
+	i2c_write((I2C_ADDR << 1) + I2C_READ); // slave address, read mode
+	x |= i2c_read_nack();
+	i2c_stop();
 	return x;
 }
 
