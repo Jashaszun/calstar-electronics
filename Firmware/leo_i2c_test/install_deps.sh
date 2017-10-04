38ec026bf05244fb8389f981fed0bd45b15e42f7
Mark Hill <markleehill@gmail.com>
2017-10-04 14:24:04 -0700

    added linux support to install_deps.sh

--- Firmware/leo_i2c_test/install_deps.sh
+++ Firmware/leo_i2c_test/install_deps.sh
@@ -1,23 +1,32 @@
-#!/bin/bash
-set -e
-set -u
+#!/bin/sh
 
-if ! command -v brew &>/dev/null; then
-  echo "Homebrew not installed, exiting..."
-  exit 0
+OS=$(uname -s)
+AVR_GCC_PATH=$(which avr-gcc)
+AVRDUDE_PATH=$(which avrdude)
+
+if [ "$AVR_GCC_PATH" = "" ] || [ "$AVRDUDE_PATH" = "" ]; then
+	printf "Resolving missing dependencies\n" >&2
+else
+	exit 0
 fi
 
-if ! command -v avr-gcc &>/dev/null; then
-  echo "Installing avr-gcc with brew"
-  brew tap osx-cross/avr
-  brew install osx-cross/avr/avr-gcc
+if [ "$OS" = "Darwin" ]; then
+	which brew || (printf "Homebrew not installed, exiting...\n" >&2 && exit 1)
+	
+	if [ "$AVR_GCC_PATH" = "" ]; then
+		brew tap osx-cross/avr && brew install osx-cross/avr/avr-gcc || exit 1
+	fi
+	
+	if [ "$AVRDUDE_PATH" = "" ]; then
+		brew install avrdude --with-usb || exit 1
+	fi
+	exit 0
+elif [ "$OS" = "Linux" ]; then
+	printf "Install the following packages:\n" >&2
+	printf "gcc-avr gdb-avr avrdude avr-libc"
+	printf "\n" >&2	
+	printf "Try:\n> apt install \`./install_deps.sh\`\n"
+	exit 1
 else
-  echo "avr-gcc already installed"
-fi
-
-if ! command -v avrdude &>/dev/null; then
-  echo "Installing avrdude with brew"
-  brew install avrdude --with-usb
-else
-  echo "avrdude already installed"
+	printf "Could not determine operating system\nI hope it's not windows...\n"
 fi
