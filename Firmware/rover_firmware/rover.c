e789e122a0f22e36f17587fd12063a8a0389b493
Sean Keegan <s.keegan@berkeley.edu>
2017-11-19 17:10:54 -0800

    copy pasted some random guys pwm example code for rover

--- Firmware/rover_firmware/rover.c
+++ Firmware/rover_firmware/rover.c
@@ -1,13 +1,45 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdint.h>
 
 #define ADC_IO DDRC
 #define ADC_PORT PORTC
 #define ADC_PIN 1
+#define	MOTOR_PWM_IO DDRB
+#define MOTOR_PWM_PORT PORTB
+#define MOTOR1_PWM_PIN 1
+#define MOTOR2_PWN_PIN 2
+#define MOTOR1_DIR_PORT PORTB
+#define MOTOR2_DIR_PORT PORTD
+#define MOTOR1_DIR_PIN 0
+#define MOTOR2_DIR_PIN 7
+
 
 int main(){
-	ADC_setup()
+	DDRB = 0x00
+	DDRC = 0x00
+
+	DDRB |= _BV(PB1)
+	TCCR1A |= _BV(COM1A1) | _BV(WGM10);
+	TCCR1B |= _BV(CS10) | _BV(WGM12);
+
+	uint8_t pwm = 0x00;
+	bool up = true;
+	for(;;) {
+
+		OCR1A = pwm;
+
+		pwm += up ? 1 : -1;
+		if (pwm == 0xff)
+			up = false;
+		else if (pwm == 0x00)
+			up = true;
+
+		_delay_ms(10);
+
+	ADC_setup();
+	}
 }
 
 //ADMUX operation sets reference voltage to AVcc, enables ADC1, and left-adjusts data registers
@@ -21,5 +53,5 @@ void ADC_setup(){
 uint16_t ADC_read(){
 	ADCSRA |= _BV(ADSC)
 	while( (ADCSRA & _BV(ADSC)) );
-	return ADC;
+	return ADCH;
 }
