088854643233c98c1f78a5341ee38cc78568a2f0
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2017-11-28 16:07:48 -0800

    accelerometer class

---
+++ Firmware/accelerometer_lis331hh_test/LIS331HH.cpp
@@ -0,0 +1,47 @@
+#include "LIS331HH.h"
+
+#include <Wire.h>
+
+LIS331HH::LIS331HH(const bool lesser_bit):
+    addr(LIS331HH_BASE_ADDRESS|lesser_bit)
+{
+    Wire.begin();
+}
+
+int16_t LIS331HH::get_x() const {
+    return (read_reg(LIS331HH_OUT_X_H) << 8) | read_reg(LIS331HH_OUT_X_L);
+}
+
+int16_t LIS331HH::get_y() const {
+    return (read_reg(LIS331HH_OUT_Y_H) << 8) | read_reg(LIS331HH_OUT_Y_L);
+}
+
+int16_t LIS331HH::get_z() const {
+    return (read_reg(LIS331HH_OUT_Z_H) << 8) | read_reg(LIS331HH_OUT_Z_L);
+}
+
+byte LIS331HH::read_reg(const byte reg) const {
+    Wire.beginTransmission(addr);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+
+    Wire.requestFrom(addr, (byte)1);
+
+    byte delay = 0;
+    while ((Wire.available() <= 0) && delay < LIS331HH_READ_TIMEOUT) {
+        ++delay;
+    }
+
+    if (delay < LIS331HH_READ_TIMEOUT) {
+        return Wire.read();
+    } else {
+        return -1;
+    }
+}
+
+void LIS331HH::write_reg(const byte reg, const byte val) const {
+    Wire.beginTransmission(addr);
+    Wire.write(reg);
+    Wire.write(val);
+    Wire.endTransmission(true);
+}
---
+++ Firmware/accelerometer_lis331hh_test/LIS331HH.h
@@ -0,0 +1,56 @@
+#ifndef LIS331HH_H
+#define LIS331HH_H
+
+#include <Arduino.h>
+
+
+#define LIS331HH_READ_TIMEOUT 50
+
+// must be bit or'd with a 0 or a 1 to result in the actual device address
+#define LIS331HH_BASE_ADDRESS     0b0011000
+
+#define LIS331HH_CTRL_REG1        0x20
+#define LIS331HH_CTRL_REG2        0x21
+#define LIS331HH_CTRL_REG3        0x22
+#define LIS331HH_CTRL_REG4        0x23
+#define LIS331HH_CTRL_REG5        0x24
+
+#define LIS331HH_HP_FILTER_RESET 0x25
+#define LIS331HH_REFERENCE       0x26
+#define LIS331HH_STATUS_REG      0x27
+
+#define LIS331HH_OUT_X_L 0x28
+#define LIS331HH_OUT_X_H 0x29
+
+#define LIS331HH_OUT_Y_L 0x2A
+#define LIS331HH_OUT_Y_H 0x2B
+
+#define LIS331HH_OUT_Z_L 0x2C
+#define LIS331HH_OUT_Z_H 0x2D
+
+#define LIS331HH_INT1_CFG        0x30
+#define LIS331HH_INT1_SOURCE     0x31
+#define LIS331HH_INT1_THS        0x32
+#define LIS331HH_INT1_DURATION   0x33
+
+#define LIS331HH_INT2_CFG        0x34
+#define LIS331HH_INT2_SOURCE     0x35
+#define LIS331HH_INT2_THS        0x36
+#define LIS331HH_INT2_DURATION   0x37
+
+class LIS331HH {
+private:
+    byte addr;
+public:
+    LIS331HH(const bool lesser_bit);
+
+    byte read_reg(const byte reg) const;
+    void write_reg(const byte reg, const byte val) const;
+
+    int16_t get_x() const;
+    int16_t get_y() const;
+    int16_t get_z() const;
+
+};
+
+#endif
