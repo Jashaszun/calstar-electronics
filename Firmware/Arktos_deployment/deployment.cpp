32dfbeae311c62aa97b829a54d3ddf96dd3e87fc
Dinesh Parimi <dinesh.parimi@live.com>
2017-12-01 11:58:34 -0800

    changed ADC to digital read in deployment

--- Firmware/Arktos_deployment/deployment.cpp
+++ Firmware/Arktos_deployment/deployment.cpp
@@ -7,9 +7,9 @@
 
 #define F_CPU 16000000
 #define ADC_THRESHOLD 512
-#define ADC_IO DDRC
-#define ADC_PORT PORTC
-#define ADC_PIN 0
+#define SIGNAL_IO DDRC
+#define SIGNAL_PORT PINC
+#define SIGNAL_PIN 0
 #define SOLENOID_IO DDRB
 #define SOLENOID_PORT PORTB
 #define SOLENOID_PIN 0
@@ -34,14 +34,14 @@ int main() {
 	4. Actuate solenoid. */
 
 	// Setup
-	ADC_IO = 0x00; // configure ADC as input
+	SIGNAL_IO = 0x00; // configure signal receiver as input
 	SOLENOID_IO = 0xFF; // configure solenoid as output
 	LED_IO = 0xFF; // configure LED as output
 	SOLENOID_PORT = 0x00;
 	LED_PORT = 0x00;
 	MPL3115A2 altimeter;
 
-	ADC_setup();
+	// ADC_setup();
 	Wire.begin();
 	altimeter.begin();
 	altimeter.setModeAltimeter();
@@ -51,7 +51,7 @@ int main() {
 	float avg = altimeter.readAltitudeFt();
 
 	// Main program thread
-	while (altimeter.readAltitudeFt() < ALT_LAUNCHED - avg) { // wait for vehicle to launch 
+	while (altimeter.readAltitudeFt() < ALT_LAUNCHED - avg) { // wait for vehicle to launch
 		avg = (avg + altimeter.readAltitudeFt()) / 2; // compute moving exponential average
 	}
 	LED_PORT = (1 << LED_PIN_RED); // set LED to red to indicate launch
@@ -63,21 +63,21 @@ int main() {
 	return 0;
 }
 
-void ADC_setup() {
-	ADMUX = (1 << REFS0) | (1 << ADLAR); // muxes ADC0, left-adjusts ADC Data Register, and sets AVCC as reference voltage
-	ADCSRA = (1 << ADEN) | (1 << ADIE) | 0b111; // Enables ADC and Conversion Complete interrupt flag, and sets prescaler to 128 for an ADC frequency of 16MHz/128 = 125kHz
-}
-
-char ADC_read() {
-	ADCSRA |= (1 << ADSC);
-	while(!(ADCSRA & (1 << ADIF))); // wait for interrupt flag
-	char result = ADCH; // get values for ADC data registers
-	ADCSRA |= (1 << ADIF); // clear interrupt flag
-	return result;
-}
+// void ADC_setup() {
+// 	ADMUX = (1 << REFS0) | (1 << ADLAR); // muxes ADC0, left-adjusts ADC Data Register, and sets AVCC as reference voltage
+// 	ADCSRA = (1 << ADEN) | (1 << ADIE) | 0b111; // Enables ADC and Conversion Complete interrupt flag, and sets prescaler to 128 for an ADC frequency of 16MHz/128 = 125kHz
+// }
+//
+// char ADC_read() {
+// 	ADCSRA |= (1 << ADSC);
+// 	while(!(ADCSRA & (1 << ADIF))); // wait for interrupt flag
+// 	char result = ADCH; // get values for ADC data registers
+// 	ADCSRA |= (1 << ADIF); // clear interrupt flag
+// 	return result;
+// }
 
 char waitForSignal() {
-	while(ADC_read() < ADC_THRESHOLD); // wait for ADC value
+	while(!(SIGNAL_PORT & (1 << SIGNAL_PIN))); // wait for ADC value
 	if (altimeter.readAltitudeFt() > ALT_LAND) { // if not on ground
 		return 0;
 	}
