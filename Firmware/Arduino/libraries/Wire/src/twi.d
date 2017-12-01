ede03a3b4c365c4b3055585285193cc44f79ba24
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2017-11-30 23:41:39 -0800

    arduino unnecesary files removed

--- Firmware/Arduino/libraries/Wire/src/twi.d
+++
@@ -1,6 +0,0 @@
-twi.o twi.d : \
- C:/Users/Jacob/Desktop/Electronics/calstar-electronics/Firmware/Arduino/hardware/arduino/avr/libraries/Wire/src/twi.c \
-  C:/Users/Jacob/Desktop/Electronics/calstar-electronics/Firmware/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
-  C:/Users/Jacob/Desktop/Electronics/calstar-electronics/Firmware/Arduino/hardware/arduino/avr/cores/arduino/binary.h \
-  C:/Users/Jacob/Desktop/Electronics/calstar-electronics/Firmware/Arduino/hardware/arduino/avr/variants/standard/pins_arduino.h \
-  C:/Users/Jacob/Desktop/Electronics/calstar-electronics/Firmware/Arduino/hardware/arduino/avr/libraries/Wire/src/twi.h
