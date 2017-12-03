f0aa1ac566a7886b5144212874dd485f777ec150
Leo Adberg <leo@adberg.com>
2017-12-03 00:00:13 -0800

    Removed prebuilt osx toolchain, now automatically installs with brew

---
+++ Firmware/toolchain/install_deps_osx.sh
@@ -0,0 +1,13 @@
+set -e
+
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+HASTAP=$(brew tap | grep osx-cross/avr | head -n 1)
+if [[ ! $HASTAP ]]; then
+	echo "Tapping osx-cross/avr"
+	brew tap osx-cross/avr
+fi
+command -v "brew ls --versions avr-gcc" >/dev/null 2>&1 || { echo >&2 "Installing avr-gcc"; brew install avr-gcc; }
+command -v "brew ls --versions avrdude" >/dev/null 2>&1 || { echo >&2 "Installing avrdude"; brew install avrdude; }
+
+BASEDIR=$(dirname "$0")
+touch $BASEDIR/INSTALLED
--- Firmware/toolchain/osx/avr-gcc/7.2.0/lib/gcc/avr/7.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --target=avr --prefix=/usr/local/Cellar/avr-gcc/7.2.0 --enable-languages=c,c++ --with-ld=/usr/local/opt/avr-binutils/bin/avr-ld --with-as=/usr/local/opt/avr-binutils/bin/avr-as --disable-nls --disable-libssp --disable-shared --disable-threads --disable-libgomp --with-dwarf2";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
