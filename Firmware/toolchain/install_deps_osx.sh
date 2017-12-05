a671315b1a1a4ff715501e475a856f6e4aeac878
Leo Adberg <leo@adberg.com>
2017-12-05 14:01:23 -0800

    Updated install scripts a bit

--- Firmware/toolchain/install_deps_osx.sh
+++ Firmware/toolchain/install_deps_osx.sh
@@ -1,5 +1,9 @@
 set -e
 
+command -v "xcode-select" >/dev/null 2>&1 || { echo >&2 "You must install Xcode"; exit 1; }
+
+xcode-select --install
+
 command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 HASTAP=$(brew tap | grep osx-cross/avr | head -n 1)
 if [[ ! $HASTAP ]]; then
