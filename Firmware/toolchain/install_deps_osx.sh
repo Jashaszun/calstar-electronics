369d74d2542ec3f99b733c042113aa8f5f95a85f
Leo Adberg <leo@adberg.com>
2017-12-05 14:04:59 -0800

    More installation fixes

--- Firmware/toolchain/install_deps_osx.sh
+++ Firmware/toolchain/install_deps_osx.sh
@@ -2,7 +2,7 @@ set -e
 
 command -v "xcode-select" >/dev/null 2>&1 || { echo >&2 "You must install Xcode"; exit 1; }
 
-xcode-select --install
+xcode-select --install && { echo >&2 "FOLLOW ONSCREEN INSTRUCTIONS THEN RE-RUN MAKE"; exit 1; }
 
 command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 HASTAP=$(brew tap | grep osx-cross/avr | head -n 1)
