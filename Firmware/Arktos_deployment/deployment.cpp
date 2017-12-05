42e7d275e2868cbb62a5f7eba7efe614b0569baa
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2017-12-05 15:02:33 -0800

    pushing launch fixes again

--- Firmware/Arktos_deployment/deployment.cpp
+++ Firmware/Arktos_deployment/deployment.cpp
@@ -19,47 +19,25 @@
 #define LED_PIN_GREEN 6
 #define LED_PIN_BLUE 7
 #define ACCEL_TOLERANCE 1
-#define ALT_LAND 50
+#define ALT_LAND 200
 #define ALT_LAUNCHED 200
 #define ACCEL_ADDR 0x00
 #define ALT_ADDR 0x60
 #define ALT_WRITE_ADDR 0xC0
 #define ALT_READ_ADDR 0xC1
-#define BASE_ALT 0
+#define BASE_ALT 185
 
-int main() {
-	/* Pseudocode for deployment sequence:
-	1. Verify vehicle has launched with altimeter
-	2. Wait until ADC input exceeds ADC threshold
-	3. Verify vehicle is at rest on the ground with altimeter
-	4. Actuate solenoid. */
+MPL3115A2 altimeter;
 
-	// Setup
-	SIGNAL_IO = 0x00; // configure signal receiver as input
-	SOLENOID_IO = 0xFF; // configure solenoid as output
-	LED_IO = 0xFF; // configure LED as output
-	SOLENOID_PORT = 0x00;
-	LED_PORT = 0x00;
-	MPL3115A2 altimeter;
-
-	// ADC_setup();
-
-	// Configure accelerometer
-	Wire.begin();
-	altimeter.begin();
-	altimeter.setModeAltimeter();
-	altimeter.setOversampleRate(7);
-	altimeter.enableEventFlags();
-
-	// Main program thread
-	while (altimeter.readAltitudeFt() - BASE_ALT < ALT_LAUNCHED); // wait for vehicle to launch
-	LED_PORT = (1 << LED_PIN_RED); // set LED to red to indicate launch
-	while (waitForSignal() == 0); // wait for ejection signal and cross-check with sensor
-	LED_PORT = (1 << LED_PIN_GREEN); // set LED to green to indicate receipt of signal
-
-	SOLENOID_PORT = (1 << SOLENOID_PIN); // trigger solenoid
-	LED_PORT = (1 << LED_PIN_BLUE) // set LED to blue to indicate completion of program
-	return 0;
+char waitForSignal() {
+	float alt;
+	while(!(SIGNAL_PORT & (1 << SIGNAL_PIN))); // wait for ADC value
+	alt = altimeter.readAltitudeFt();
+	Serial.println(alt);
+	if (alt - BASE_ALT > ALT_LAND) { // if not on ground
+		return 0;
+	}
+	return 1; // landed!
 }
 
 // void ADC_setup() {
@@ -75,10 +53,37 @@ int main() {
 // 	return result;
 // }
 
-char waitForSignal() {
-	while(!(SIGNAL_PORT & (1 << SIGNAL_PIN))); // wait for ADC value
-	if (altimeter.readAltitudeFt() > ALT_LAND) { // if not on ground
-		return 0;
-	}
-	return 1; // landed!
+int main() {
+	/* Pseudocode for deployment sequence:
+	1. Verify vehicle has launched with altimeter
+	2. Wait until ADC input exceeds ADC threshold
+	3. Verify vehicle is at rest on the ground with altimeter
+	4. Actuate solenoid. */
+
+	// Setup
+	SIGNAL_IO = 0x00; // configure signal receiver as input
+	SOLENOID_IO = 0xFF; // configure solenoid as output
+	LED_IO = 0xFF; // configure LED as output
+	SOLENOID_PORT = 0x00;
+	LED_PORT = 0x00;
+	// ADC_setup();
+
+	init();
+	Serial.begin(19200);
+	// Configure altimeter
+	Wire.begin();
+	altimeter.begin();
+	altimeter.setModeAltimeter();
+	altimeter.setOversampleRate(7);
+	altimeter.enableEventFlags();
+
+	// Main program thread
+	while (altimeter.readAltitudeFt() - BASE_ALT < ALT_LAUNCHED) Serial.println(altimeter.readAltitudeFt()); // wait for vehicle to launch
+	LED_PORT = (1 << LED_PIN_RED); // set LED to red to indicate launch
+	while (waitForSignal() == 0) Serial.println(altimeter.readAltitudeFt()); // wait for ejection signal and cross-check with sensor
+	LED_PORT = (1 << LED_PIN_GREEN); // set LED to green to indicate receipt of signal
+
+	SOLENOID_PORT = (1 << SOLENOID_PIN); // trigger solenoid
+	LED_PORT = (1 << LED_PIN_BLUE); // set LED to blue to indicate completion of program
+	return 0;
 }
