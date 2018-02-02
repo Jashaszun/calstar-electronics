b27724c060f4bb44646bf8ff848a91def36817bd
Jacob Posner <jashaszun123@gmail.com>
2018-02-02 14:52:00 -0800

    Updated new deployment sensor test file.

--- Firmware/Arktos_deployment/sensors_test.cpp
+++ Firmware/Arktos_deployment/sensors_test.cpp
@@ -12,7 +12,8 @@ int main() {
   // Setup
   init(); // always include this when using Arduino.h!
   Serial.begin(BAUD_RATE);
-  Serial.println("Serial interface initialized with a baud rate of %i", BAUD_RATE);
+  Serial.print("Serial interface initialized with a baud rate of ");
+  Serial.println((int)BAUD_RATE);
   MPL3115A2 altimeter;
   LIS331HH accelerometer(0); // tie SA0 to ground
 
@@ -22,7 +23,7 @@ int main() {
   altimeter.setOversampleRate(7);
   altimeter.enableEventFlags();
 
-  Accelerometer setup
+  // Accelerometer setup
   accelerometer.write_reg(LIS331HH_CTRL_REG1, 0x3F); // Normal power mode, 1000 Hz data rate, x y and z axes enabled
   accelerometer.write_reg(LIS331HH_CTRL_REG4, 0x90); // Block data update, +- 12g scale
 
@@ -33,7 +34,7 @@ int main() {
     float alt = altimeter.readAltitudeFt();
 
     char outbuf[100];
-    sprintf(outbuf, "Ac: x: %+07i y: %+07i z: %+07i  Al: %+07i\n", (int)(1000*accelX), (int)(1000*accelY), (int)(1000*accelZ), (int)(alt-currentAltZero));
+    sprintf(outbuf, "Ac: x: %+07i y: %+07i z: %+07i  Al: %+07i\n", (int)(1000*accelX), (int)(1000*accelY), (int)(1000*accelZ), (int)(alt));
     Serial.println(outbuf);
   }
 }
