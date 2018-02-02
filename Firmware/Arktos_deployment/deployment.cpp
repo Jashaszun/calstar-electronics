a1c23baa278bd0d6d280691a145b08873181a8fe
Dinesh Parimi <dinesh.parimi@live.com>
2018-02-01 20:40:29 -0800

    First firmware updates for deployment v2

--- Firmware/Arktos_deployment/deployment.cpp
+++ Firmware/Arktos_deployment/deployment.cpp
@@ -2,22 +2,28 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <Arduino.h>
 #include "Wire.h"
 #include "SparkFunMPL3115A2.h"
 
 #define F_CPU 16000000
-#define ADC_THRESHOLD 512
-#define SIGNAL_IO DDRC
-#define SIGNAL_PORT PINC
-#define SIGNAL_PIN 0
-#define SOLENOID_IO DDRB
-#define SOLENOID_PORT PORTB
-#define SOLENOID_PIN 0
+#define LVDS_IO DDRD
+#define LVDS_PORT PIND
+#define LVDS_PIN 2
+#define CHARGE_IO DDRC
+#define CHARGE_PORT PORTC
+#define CHARGE_PIN 1
 #define LED_IO DDRD
 #define LED_PORT PORTD
 #define LED_PIN_RED 5
 #define LED_PIN_GREEN 6
 #define LED_PIN_BLUE 7
+#define BUZZER_IO DDRB
+#define BUZZER_PORT PORTB
+#define BUZZER_PIN 1
+#define CONTINUITY_IO DDRD
+#define CONTINUITY_PORT PIND
+#define CONTINUITY_PIN 4
 #define ACCEL_TOLERANCE 1
 #define ALT_LAND 50
 #define ALT_LAUNCHED 200
@@ -35,14 +41,14 @@ int main() {
 	4. Actuate solenoid. */
 
 	// Setup
-	SIGNAL_IO = 0x00; // configure signal receiver as input
-	SOLENOID_IO = 0xFF; // configure solenoid as output
-	LED_IO = 0xFF; // configure LED as output
-	SOLENOID_PORT = 0x00;
-	LED_PORT = 0x00;
+	// Signal receiver and continuity default to input
+	LED_IO = (1 << LED_PIN_RED) | (1 << LED_PIN_GREEN) | (1 << LED_PIN_BLUE); // configure LEDs as output
+	CHARGE_IO = 0xFF; // configure black powder as output
+	BUZZER_IO = 0xFF; // configure buzzer as output
+	LED_PORT = 0x00; // set LEDs to low
+	CHARGE_PORT = 0x00; // set charge to low
+	BUZZER_PORT = 0x00; // set buzzer to low
 	MPL3115A2 altimeter;
-
-	// ADC_setup();
 
 	// Configure accelerometer
 	Wire.begin();
@@ -51,34 +57,50 @@ int main() {
 	altimeter.setOversampleRate(7);
 	altimeter.enableEventFlags();
 
+	// Initialize timer vars
+	unsigned long* start_time;
+	unsigned long* buzzer_time;
+	short* buzzer;
+	*start_time = millis();
+	*buzzer_time = micros();
+	*buzzer = 0;
+
 	// Main program thread
-	while (altimeter.readAltitudeFt() - BASE_ALT < ALT_LAUNCHED); // wait for vehicle to launch
+	while (altimeter.readAltitudeFt() - BASE_ALT < ALT_LAUNCHED) { // wait for vehicle to launch
+		beep(start_time, buzzer_time, buzzer);
+	}
 	LED_PORT = (1 << LED_PIN_RED); // set LED to red to indicate launch
-	while (waitForSignal() == 0); // wait for ejection signal and cross-check with sensor
+	while (waitForSignal() == 0) { // wait for ejection signal and cross-check with sensor
+		beep(start_time, buzzer_time, buzzer);
+	}
 	LED_PORT = (1 << LED_PIN_GREEN); // set LED to green to indicate receipt of signal
 
-	SOLENOID_PORT = (1 << SOLENOID_PIN); // trigger solenoid
-	LED_PORT = (1 << LED_PIN_BLUE) // set LED to blue to indicate completion of program
+	CHARGE_PORT = (1 << CHARGE_PIN); // trigger solenoid
+	LED_PORT = (1 << LED_PIN_BLUE); // set LED to blue to indicate completion of program
 	return 0;
 }
 
-// void ADC_setup() {
-// 	ADMUX = (1 << REFS0) | (1 << ADLAR); // muxes ADC0, left-adjusts ADC Data Register, and sets AVCC as reference voltage
-// 	ADCSRA = (1 << ADEN) | (1 << ADIE) | 0b111; // Enables ADC and Conversion Complete interrupt flag, and sets prescaler to 128 for an ADC frequency of 16MHz/128 = 125kHz
-// }
-//
-// char ADC_read() {
-// 	ADCSRA |= (1 << ADSC);
-// 	while(!(ADCSRA & (1 << ADIF))); // wait for interrupt flag
-// 	char result = ADCH; // get values for ADC data registers
-// 	ADCSRA |= (1 << ADIF); // clear interrupt flag
-// 	return result;
-// }
-
 char waitForSignal() {
-	while(!(SIGNAL_PORT & (1 << SIGNAL_PIN))); // wait for ADC value
-	if (altimeter.readAltitudeFt() > ALT_LAND) { // if not on ground
+	while(!(LVDS_PORT & (1 << LVDS_PIN))); // wait for ADC value
+	if (altimeter.readAltitudeFt() - BASE_ALT > ALT_LAND) { // if not on ground
 		return 0;
 	}
 	return 1; // landed!
+}
+
+void beep(unsigned long* start_time, unsigned long* buzzer_time, short* buzzer) {
+	unsigned long current_time_ms = millis();
+	unsigned long current_time_us = micros();
+	if (current_time_us - *buzzer_time >= 500) {
+		*buzzer_time = current_time_us;
+		// buzzer frequency = 1/(2*500us) = 1000 Hz
+		if ((current_time_ms - *start_time <= 400) && (detectContinuity())) {
+			*buzzer = !(*buzzer);
+			BUZZER_PORT = (*buzzer << BUZZER_PIN);
+		}
+	}
+}
+
+short detectContinuity() {
+	return (CONTINUITY_PORT & (1 << CONTINUITY_PIN));
 }
