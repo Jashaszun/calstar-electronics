8ce1d41daf2a714f4fa1541d8563b23025efe939
Leo Adberg <leo@adberg.com>
2018-02-19 21:37:00 -0800

    Updated String

--- FirmwareV2/Kresge/String.cpp
+++ FirmwareV2/Kresge/String.cpp
@@ -1,111 +1,110 @@
 #include "String.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // substring, concatenation (+), contents char[] that dynamically expands, indexing, cstr
 // casting int to string, floats
 // string constructor for char*
 /* A lightweight version of WString.cpp in the Arduino libraries */
 
-inline void String::init() {
-	buffer = NULL;
-	length = 0;
+void String::init(const char* cstr) {
+	// Copy the contents of cstr into buffer
+	_length = strlen(cstr);
+	_buffer = (char*)malloc(_length + 1);
+	strcpy(_buffer, cstr);
 }
 
+// Just make space, don't write
+void String::init(size_t len) {
+	_length = len;
+	_buffer = (char*)malloc(_length + 1);
+}
+
+// INTERNAL USE
+String::String() { }
+
 String::String(const char* cstr) {
-	init();
-	// Copy the contents of cstr into buffer
-	strcpy(buffer, cstr);
+	init(cstr);
 }
 
 String::String(const String& value) {
-	init();
-	*this = value;
+	init(value.buffer());
 }
 
 String::String(const char c) {
-	init();
 	char buf[2];
 	buf[0] = c;
 	buf[1] = 0;
-	buffer = buf;
-	// do i need to free anything?
+	init(buf);
 }
 
 String::String(int value) {
-	init();
-	char buf[2 + 8 * sizeof(int)];
+	char buf[12];
 	// converts int to string
-	itoa(value, buf, 10);
-	buffer = buf;
+	sprintf(buf, "%d", value);
+	init(buf);
 }
 
 String::String(unsigned int value) {
-	init();
+	char buf[12];
+	// converts unsigned int to string
+	sprintf(buf, "%u", value);
+	init(buf);
 }
 
-String::String(long value) {
-	init();
-}
+// String::String(long value) {
+// 	init();
+// }
 
-String::String(unsigned long value) {
-	init();
-}
+// String::String(unsigned long value) {
+// 	init();
+// }
 
-String::String(float value) {
-	init();
-}
+// String::String(float value) {
+// 	init();
+// }
 
-String::String(double value) {
-	init();
-}
+// String::String(double value) {
+// 	init();
+// }
 
 String::~String() {
-	init();
+	free(_buffer);
 }
 
-class String {
+int String::length() const {
+	return _length;
+}
 
-public:
-	// Constructors
-	String(const char *cstr);
-	String(const String& value);
-	String(const char c);
-	String(int value);
-	String(unsigned int value);
-	String(long value);
-	String(unsigned long value);
-	String(float value);
-	String(double value);
-	~String();
+char* String::c_str() {
+	return _buffer;
+}
 
-	int length();
+const char* String::buffer() const {
+	return _buffer;
+}
 
-	String& substring(unsigned int start, unsigned int end);
-	char charAt(unsigned int index) const;
-	void setCharAt(unsigned int index, char c);
-	char operator [] (unsigned int index) const;
-	char& operator [] (unsigned int index);
+String String::substr(unsigned int start, unsigned int len) {
+	String newstr;
+	newstr.init(len);
+	memcpy(this->_buffer, newstr._buffer, len);
+	newstr._buffer[len] = 0;
+	return newstr;
+}
 
-	int compareTo(const String &s) const;
-	bool equals(const String &s) const;
-	bool equals(const char *cstr) const;
-	bool operator == (const String &rhs) const {return equals(rhs);}
-	bool operator == (const char *cstr) const {return equals(cstr);}
-	bool operator != (const String &rhs) const {return !equals(rhs);}
-	bool operator != (const char *cstr) const {return !equals(cstr);}
-	bool operator <  (const String &rhs) const;
-	bool operator >  (const String &rhs) const;
-	bool operator <= (const String &rhs) const;
-	bool operator >= (const String &rhs) const;
-	bool equalsIgnoreCase(const String &s) const;
-	bool startsWith( const String &prefix) const;
-	bool startsWith(const String &prefix, unsigned int offset) const;
-	bool endsWith(const String &suffix) const;
+void String::setCharAt(unsigned int index, char c) {
+	_buffer[index] = c;
+}
 
-	String& operator + (const String& left, const String& right);
+char String::operator[](unsigned int index) const {
+	return _buffer[index];
+}
 
-private:
-	int length; // should be buffer length - 1, because of null terminator
-	char* buffer;
-	void init();
-
+String String::operator+ (const String& other) const {
+	String newstr;
+	newstr.init(_length + other.length());
+	memcpy((void*)this->buffer(), newstr.buffer(), _length);
+	memcpy((void*)other.buffer(), newstr.buffer() + _length, other.length());
+	newstr.c_str()[newstr.length()] = 0;
+	return newstr;
 }
--- FirmwareV2/Kresge/String.h
+++ FirmwareV2/Kresge/String.h
@@ -1,4 +1,5 @@
-#include "String.h"
+#include <stddef.h>
+#include <string.h>
 
 /* Mostly copied from WString.h in the Arduino libraries */
 
@@ -11,40 +12,45 @@ public:
 	String(const char c);
 	String(int value);
 	String(unsigned int value);
-	String(long value);
-	String(unsigned long value);
-	String(float value);
-	String(double value);
+	// String(long value);
+	// String(unsigned long value);
+	// String(float value);
+	// String(double value);
 	~String();
 
-	int length();
+	int length() const;
+	char* c_str();
 
-	String& substring(unsigned int start, unsigned int end);
-	char charAt(unsigned int index) const;
+	String substr(unsigned int start, unsigned int len);
+	// char charAt(unsigned int index) const;
 	void setCharAt(unsigned int index, char c);
-	char operator [] (unsigned int index) const;
-	char& operator [] (unsigned int index);
+	char operator[] (unsigned int index) const;
+	// char& operator [] (unsigned int index);
 
-	int compareTo(const String &s) const;
-	bool equals(const String &s) const;
-	bool equals(const char *cstr) const;
-	bool operator == (const String &rhs) const {return equals(rhs);}
-	bool operator == (const char *cstr) const {return equals(cstr);}
-	bool operator != (const String &rhs) const {return !equals(rhs);}
-	bool operator != (const char *cstr) const {return !equals(cstr);}
-	bool operator <  (const String &rhs) const;
-	bool operator >  (const String &rhs) const;
-	bool operator <= (const String &rhs) const;
-	bool operator >= (const String &rhs) const;
-	bool equalsIgnoreCase(const String &s) const;
-	bool startsWith( const String &prefix) const;
-	bool startsWith(const String &prefix, unsigned int offset) const;
-	bool endsWith(const String &suffix) const;
+	// int compareTo(const String &s) const;
+	// bool equals(const String &s) const;
+	// bool equals(const char *cstr) const;
+	bool operator== (const String &rhs) const;
+	// bool operator== (const char *cstr) const {return equals(cstr);}
+	bool operator!= (const String &rhs) const;
+	// bool operator!= (const char *cstr) const {return !equals(cstr);}
+	// bool operator<  (const String &rhs) const;
+	// bool operator>  (const String &rhs) const;
+	// bool operator<= (const String &rhs) const;
+	// bool operator>= (const String &rhs) const;
+	// bool equalsIgnoreCase(const String &s) const;
+	// bool startsWith( const String &prefix) const;
+	// bool startsWith(const String &prefix, unsigned int offset) const;
+	// bool endsWith(const String &suffix) const;
 
-	String& operator + (const String& left, const String& right);
+	String operator+ (const String& other) const;
 
 private:
-	int length; // should be buffer length - 1, because of null terminator
-	char* buffer;
-	void init();
-}
+	unsigned int _length; // should be buffer length - 1, because of null terminator
+	char* _buffer;
+	String(); // INTERNAL USE ONLY
+	void init(const char *cstr);
+	void init(size_t len);
+	const char* buffer() const;
+
+};
