2114e0cd063c37afb65b53ad123440eb24200723
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2018-02-22 17:30:00 -0800

    changed stack to queue

--- FirmwareV2/Kresge/UART.cpp
+++ FirmwareV2/Kresge/UART.cpp
@@ -3,52 +3,98 @@
 #include <string.h>
 #include <avr/interrupt.h>
 
-_Serial::_Stack::_Stack():
-  nextTop(_SERIAL_BUF_SIZE - 1) {
+_Serial::_CircQueue::_CircQueue():
+    nextFirst(0),
+    nextLast(0),
+    size(0) {
 
 }
 
-bool _Serial::_Stack::empty() const {
-  return nextTop == _SERIAL_BUF_SIZE - 1;
+bool _Serial::_CircQueue::empty() const {
+    return size == 0;
 }
 
-bool _Serial::_Stack::full() const {
-  return nextTop == -1;
+bool _Serial::_CircQueue::full() const {
+    return size == _SERIAL_BUF_SIZE;
 }
 
-bool _Serial::_Stack::put(uint8_t byte) {
-  if (full()) {
-    return false;
-  } else {
-    buf[nextTop] = byte;
-    --nextTop;
-    return true;
-  }
+uint8_t decrement(uint8_t index) {
+    --index;
+    if (index < 0) {
+        return _SERIAL_BUF_SIZE - 1;
+    } else {
+        return index;
+    }
 }
 
-int16_t _Serial::_Stack::pop() {
-  if (empty()) {
-    return -1;
-  } else {
-    ++nextTop;
-    return buf[nextTop - 1];
-  }
+uint8_t increment(uint8_t index) {
+    return (index + 1) % _SERIAL_BUF_SIZE;
 }
 
-int16_t _Serial::_Stack::peek() const {
-  if (empty()) {
-    return -1;
-  } else {
-    return buf[nextTop - 1];
-  }
+bool _Serial::_CircQueue::putFirst(uint8_t byte) {
+    if (full()) {
+        return false;
+    } else {
+        ++size;
+        buf[nextFirst] = byte;
+        nextFirst = decrement(index);
+        return true;
+    }
 }
 
-uint8_t _Serial::_Stack::size() const {
-  return _SERIAL_BUF_SIZE - nextTop - 1;
+bool _Serial::_CircQueue::putLast(uint8_t byte) {
+    if (full()) {
+        return false;
+    } else {
+        ++size;
+        buf[nextLast] = byte;
+        nextLast = increment(index);
+        return true;
+    }
+}
+
+int16_t _Serial::_CircQueue::popFirst() {
+    if (empty()) {
+        return -1;
+    } else {
+        --size;
+        nextFirst = increment(nextFirst);
+        return buf[nextFirst];
+    }
+}
+
+int16_t _Serial::_CircQueue::popLast() {
+    if (empty()) {
+        return -1;
+    } else {
+        --size;
+        nextLast = decrement(nextLast);
+        return buf[nextLast];
+    }
+}
+
+int16_t _Serial::_CircQueue::peekFirst() const {
+    if (empty()) {
+        return -1;
+    } else {
+        return buf[increment(nextFirst)];
+    }
+}
+
+int16_t _Serial::_CircQueue::peekLast() const {
+    if (empty()) {
+        return -1;
+    } else {
+        return buf[decrement(nextLast)];
+    }
+}
+
+uint8_t _Serial::_CircQueue::size() const {
+    return size;
 }
 
 _Serial::_Serial():
-  timeout(_SERIAL_DEFAULT_TIMEOUT_MS) {
+    timeout(_SERIAL_DEFAULT_TIMEOUT_MS) {
 
 }
 
@@ -57,44 +103,44 @@ void _Serial::begin(int baudRate) {
 }
 
 uint8_t _Serial::available() const {
-  return rxBuf.size();
+    return rxBuf.size();
 }
 
 int16_t _Serial::peek() const {
-  return rxBuf.peek();
+    return rxBuf.peek();
 }
 
 int16_t _Serial::readByte() {
-  return rxBuf.pop();
+    return rxBuf.pop();
 }
 
 uint8_t min(uint8_t a, uint8_t b) {
-  if (a < b) {
-    return a;
-  } else {
-    return b;
-  }
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
 uint8_t _Serial::readBytes(uint8_t *buf, uint8_t len) {
-  // time(0) and such return time in seconds. need Timer class implemented before this
-  // can be completed
+    // time(0) and such return time in seconds. need Timer class implemented before this
+    // can be completed
 
 
-  // time_t last_recieved = time(0);
-  // uint8_t i = 0;
-  // while (i != len) {
-  //   if (available() > 0) {
-  //     buf[i] = readByte();
-  //     ++i;
-  //     last_recieved = time(0);
-  //   }
-  //   if (difftime(time(0), last_recieved) > timeout) {
-  //     break;
-  //   }
-  // }
-  // return i;
-  return 0;
+    // time_t last_recieved = time(0);
+    // uint8_t i = 0;
+    // while (i != len) {
+    //     if (available() > 0) {
+    //         buf[i] = readByte();
+    //         ++i;
+    //         last_recieved = time(0);
+    //     }
+    //     if (difftime(time(0), last_recieved) > timeout) {
+    //         break;
+    //     }
+    // }
+    // return i;
+    return 0;
 }
 
 void _Serial::write(uint8_t byte) {
@@ -106,11 +152,11 @@ uint8_t _Serial::write(const uint8_t *buf, uint8_t len) {
 }
 
 void _Serial::print(const String &str) {
-  write((uint8_t *)str.const_c_str(), str.length());
+    write((uint8_t *)str.const_c_str(), str.length());
 }
 
 void _Serial::setTimeout(uint32_t timeout) {
-  this->timeout = timeout;
+    this->timeout = timeout;
 }
 
 void _Serial::_rx_isr_() {
@@ -124,9 +170,9 @@ void _Serial::_tx_isr_() {
 // these vector names are specifically for the ATmega328P.
 // For other devices, should probably do #defining in the .h to make everything uniform
 ISR(USART_TX_vect) {
-  Serial._tx_isr_();
+    Serial._tx_isr_();
 }
 
 ISR(USART_RX_vect) {
-  Serial._rx_isr_();
+    Serial._rx_isr_();
 }
--- FirmwareV2/Kresge/UART.h
+++ FirmwareV2/Kresge/UART.h
@@ -7,48 +7,53 @@
 class _Serial {
 private:
 
-  class _Stack {
-  public:
-    uint8_t buf[_SERIAL_BUF_SIZE];
-    uint8_t nextTop;
-    _Stack();
-    bool full() const;
-    bool empty() const;
-    bool put(uint8_t byte);
-    int16_t pop();
-    int16_t peek() const;
-    uint8_t size() const;
-  };
+    class _CircQueue {
+    public:
+        uint8_t buf[_SERIAL_BUF_SIZE];
+        uint8_t size;
+        uint8_t nextFirst;
+        uint8_t nextLast;
+        _CircQueue();
+        bool full() const;
+        bool empty() const;
+        bool putFirst(uint8_t byte);
+        bool putLast(uint8_t byte);
+        int16_t popFirst();
+        int16_t popLast();
+        int16_t peekFirst() const;
+        int16_t peekLast() const;
+        uint8_t size() const;
+    };
 
-  _Stack rxBuf;
-  _Stack txBuf;
+    _CircQueue rxBuf;
+    _CircQueue txBuf;
 
-  uint32_t timeout;
+    uint32_t timeout;
 public:
-  _Serial();
+    _Serial();
 
-  void begin(int baudRate);
+    void begin(int baudRate);
 
-  uint8_t available() const;
+    uint8_t available() const;
 
-  int16_t peek() const;
+    int16_t peek() const;
 
-  // Returns a single byte
-  int16_t readByte();
+    // Returns a single byte
+    int16_t readByte();
 
-  // Blocks, until either timeout or len number of bytes are read
-  uint8_t readBytes(uint8_t *buf, uint8_t len);
+    // Blocks, until either timeout or len number of bytes are read
+    uint8_t readBytes(uint8_t *buf, uint8_t len);
 
-  void write(uint8_t byte);
+    void write(uint8_t byte);
 
-  uint8_t write(const uint8_t *buf, uint8_t len);
+    uint8_t write(const uint8_t *buf, uint8_t len);
 
-  void print(const String &str);
+    void print(const String &str);
 
-  void setTimeout(uint32_t timeout);
+    void setTimeout(uint32_t timeout);
 
-  // pls no use
-  void _rx_isr_();
-  void _tx_isr_();
+    // pls no use
+    void _rx_isr_();
+    void _tx_isr_();
 };
 _Serial Serial;
