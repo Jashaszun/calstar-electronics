e87707195c0eb6d5ad51a6d5b8033cd0724282bc
Dinesh Parimi <dinesh.parimi@live.com>
2018-02-22 16:27:57 -0800

    untested SPI complete

--- FirmwareV2/Kresge/SPI.h
+++ FirmwareV2/Kresge/SPI.h
@@ -1,1 +1,7 @@
 #include "DIO.h"
+
+struct Slave;
+
+short SPI_init(short clock_div); // Initializes SPI interface with clock speed f_osc / clock_div
+char read_data(Slave slave);
+void write_data(Slave slave, char t_byte);
