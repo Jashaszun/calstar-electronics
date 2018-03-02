2e596fc4a4a1453f545ebb84a9e18b25c8be567e
noloerino <jhshi07@gmail.com>
2018-03-02 14:17:42 -0800

    Nonblocking beeping | Vax Vobiscum

--- Firmware/Arktos_deployment/deployment_fsm.cpp
+++ Firmware/Arktos_deployment/deployment_fsm.cpp
@@ -43,7 +43,11 @@ enum LaunchState {
 
 char wait_for_signal();
 
-void beep();
+void process_beep(); // runs each iteration of the while loop and flips voltage if necessary
+void beep_if_continuity(); // every 2 seconds, check for continuity - if there's continuity, beep for half a second
+void start_beep(long duration_us); // runs a beep that lasts for `duration` ms,
+								   // if another call is made that will override any existing beep
+void stop_beep();
 
 short black_powder_present();
 short deployment_signal();
@@ -133,11 +137,8 @@ int main() {
 					} else if (command == "CONTINUITY\n") {
 						Serial.println(black_powder_present() ? "Continuity - black powder present" : "Disconnected - black powder absent");
 					} else if (command == "BEEP\n") {
-						Serial.print("Beeping... ");
-						Serial.flush();
-
-						for (int i = 0; i < 5; i++) beep();
-
+						Serial.print("Starting beep for 0.5s");
+						start_beep(500000);
 						Serial.println("done.");
 					} else if (command == "ALTIMETER") {
 						printAltimeterReading = !printAltimeterReading;
@@ -148,20 +149,9 @@ int main() {
 				}
 				break;
 			case LAUNCH:
+				beep_if_continuity();
 				switch(launch_state) {
 					case PAD:
-						setLEDs(1, 1, 0);
-						for (int i = 0; i < 3; i++) {
-							setLEDs(1, 1, 1);
-							beep();
-							setLEDs(1, 1, 0);
-							delay(1000);
-						}
-						setLEDs(0, 1, 1);
-						for (int i = 0; i < 3; i++) {
-							if (black_powder_present()) beep();
-							else delay(1000);
-						}
 						if (launched()) launch_state = FLIGHT;
 						break;
 					case FLIGHT:
@@ -170,6 +160,7 @@ int main() {
 						break;
 					case LANDED:
 						setLEDs(0, 1, 0); // indicates landing condition met
+						// TODO add wait_for_signal process
 						if (deployment_signal()) launch_state = SIGNAL_RECEIVED;
 						break;
 					case SIGNAL_RECEIVED:
@@ -182,14 +173,12 @@ int main() {
 						break;
 					case DEPLOYED:
 						setLEDs(1, 1, 1); // program complete
-						if (black_powder_present()) {
-							beep();
-						}
-						delay(1000);
 						break;
 				}
 				break;
 		}
+		// check if a beep should be made
+		process_beep();
 		// Read serial and radio, check to enter test mode
 		if (Serial.available()) {
 		  command_available = true;
@@ -222,9 +211,7 @@ char wait_for_signal() {
 		} else {
 			count = 0;
 		}
-		if (black_powder_present()) {
-			beep();
-		}
+		beep_if_continuity();
 	}
 	if (landed()) { // landed!
 		return 1;
@@ -232,18 +219,45 @@ char wait_for_signal() {
 	return 0; // not on ground
 }
 
-void beep() {
-	uint8_t buzzer = 0;
-	unsigned long start_time_us = micros();
-	unsigned long last_flip = start_time_us;
-	//digitalWrite(BUZZER_PIN, buzzer);
-	while (micros() - start_time_us < 1000000) {
-		if (micros() - last_flip >= 500) {
-			buzzer = !buzzer;
-			last_flip = micros();
-			digitalWrite(BUZZER_PIN, buzzer);
-			//BUZZER_PORT = (buzzer << BUZZER_PIN);
-		}
+// ** PROCESSING BEEPING **
+bool beeping = false; // whether or not process_beep should attempt to emit noise
+unsigned long last_buzzer_flip = 0; // the time of the last flip of the buzzer voltage
+unsigned long last_beep_start = 0; // the most recent time where start_beep was called
+unsigned long curr_beep_duration = 0; // the duration in us of the current beep
+bool buzzer_state = 0; // the next voltage to be written to the buzzer pin
+
+/*
+This function should be called on every iteration of the while loop.
+If a beep does not need to be done, then it just returns.
+Otherwise, if enough time has passed since the last flip of the buzzer voltage, it will flip the
+voltage on the buzzer. 
+*/
+void process_beep() {
+	if (beeping && micros() - last_buzzer_flip >= 500) { // beeps at 500 Hz
+		buzzer_state = !buzzer_state;
+		last_buzzer_flip = micros();
+		digitalWrite(BUZZER_PIN, buzzer_state);
+	}
+	if (micros() - last_beep_start > curr_beep_duration) {
+		stop_beep();
+	}
+}
+
+void start_beep(long duration_us) {
+	beeping = true;
+	last_beep_start = micros();
+	curr_beep_duration = duration_us;
+}
+
+void stop_beep() {
+	beeping = false;
+}
+
+unsigned long last_continuity_check = 0; // last time continuity check was performed
+void beep_if_continuity() {
+	if (micros() - last_continuity_check >= 2000000 // check every two seconds
+			&& black_powder_present()) { // continuity detected
+		start_beep(500000); // beep for 0.5 seconds
 	}
 }
 
@@ -259,7 +273,6 @@ void setLEDs(uint8_t red, uint8_t green, uint8_t blue) {
 }
 
 short deployment_signal() {
-	// return !(RECEIVER_PORT & (1 << RECEIVER_PIN));
 	return !digitalRead(RECEIVER_PIN);
 }
 
@@ -267,6 +280,7 @@ short launched() {
 	static unsigned short counter = 0;
 	if (altimeter.readAltitudeFt() - base_alt < ALT_LAUNCHED) {
 		counter = 0;
+		return 0;
 	} else {
 		counter ++;
 		return (counter >= VERIF_SAMPLES);
@@ -277,6 +291,7 @@ short landed() {
 	static unsigned short counter = 0;
 	if (altimeter.readAltitudeFt() - base_alt > ALT_LAND) {
 		counter = 0;
+		return 0;
 	} else {
 		counter ++;
 		return (counter >= VERIF_SAMPLES);
