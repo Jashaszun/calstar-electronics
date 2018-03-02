8e872880cfd81929077bfce693b5ad552735b752
Jacob Posner <jashaszun123@gmail.com>
2018-03-01 22:33:57 -0800

    Added buzzing back to deployment.

--- Firmware/Arktos_deployment/deployment_fsm.cpp
+++ Firmware/Arktos_deployment/deployment_fsm.cpp
@@ -3,30 +3,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <Arduino.h>
-// #include <Wire.h>
-// #include <SparkFunMPL3115A2.h>
+#include <Wire.h>
+#include <SparkFunMPL3115A2.h>
 
 #define F_CPU 16000000
-#define RECEIVER_IO DDRD
-#define RECEIVER_PORT PIND
 #define RECEIVER_PIN 3
-#define TRANSMITTER_IO DDRD
-#define TRANSMITTER_PORT PORTD
 #define TRANSMITTER_PIN 2
-#define CHARGE_IO DDRC
-#define CHARGE_PORT PORTC
-#define CHARGE_PIN 1
 #define BLACK_POWDER_PIN_ARDUINO A1
-#define LED_IO DDRD
-#define LED_PORT PORTD
 #define LED_PIN_RED 5
 #define LED_PIN_GREEN 7
 #define LED_PIN_BLUE 6
-#define BUZZER_IO DDRB
-#define BUZZER_PORT PORTB
-#define BUZZER_PIN 1
-#define CONTINUITY_IO DDRD
-#define CONTINUITY_PORT PIND
+#define BUZZER_PIN 9
 #define CONTINUITY_PIN 4
 #define ACCEL_TOLERANCE 1
 #define ALT_LAND 200
@@ -39,7 +26,7 @@
 #define VERIF_SAMPLES 20
 
 // Altimeter
-// MPL3115A2 altimeter;
+MPL3115A2 altimeter;
 
 enum State {
 	LAUNCH = 0,
@@ -83,23 +70,17 @@ int main() {
 	// Setup
 	init(); // always do this when using Arduino.h!
 	Serial.begin(19200);
-	// LVDS receiver and continuity default to input
-	// LED_IO = (1 << LED_PIN_RED) | (1 << LED_PIN_GREEN) | (1 << LED_PIN_BLUE); // configure LEDs as output
-	// TRANSMITTER_IO |= (1 << TRANSMITTER_PIN); // configure LVDS transmitter as output
-	//CHARGE_IO = 0xFF; // configure black powder as output
-	BUZZER_IO = 0xFF; // configure buzzer as output
-	// LED_PORT = 0x00; // set LEDs and transmitter output to low
-	//CHARGE_PORT = 0x00; // set charge to low
-	BUZZER_PORT = 0x00; // set buzzer to low
 
 	pinMode(RECEIVER_PIN, INPUT_PULLUP);
 	pinMode(TRANSMITTER_PIN, OUTPUT);
 	pinMode(LED_PIN_BLUE, OUTPUT);
 	pinMode(LED_PIN_GREEN, OUTPUT);
 	pinMode(LED_PIN_RED, OUTPUT);
-	digitalWrite(TRANSMITTER_PIN, LOW);
+	digitalWrite(TRANSMITTER_PIN, 0);
 	pinMode(BLACK_POWDER_PIN_ARDUINO, OUTPUT);
-	digitalWrite(BLACK_POWDER_PIN_ARDUINO, LOW);
+	digitalWrite(BLACK_POWDER_PIN_ARDUINO, 0);
+
+	pinMode(BUZZER_PIN, OUTPUT);
 
 	// Configure accelerometer
 	// Wire.begin();
@@ -114,27 +95,27 @@ int main() {
 		switch (state) {
 			case TEST_MODE:
 				if (command_available) {
-					if (command == "EXIT") {
+					if (command == "EXIT\n") {
 						Serial.println("Deployment exiting test mode.");
 						state = LAUNCH;
-					} else if (command.length() == 7 && command.substring(0, 4) == "led ") {
+					} else if (command.length() == 8 && command.substring(0, 4) == "led ") {
 						setLEDs(command[4] == '1', command[5] == '1', command[6] == '1');
-					} else if (command == "LVDS_RECEIVE") {
+					} else if (command == "LVDS_RECEIVE\n") {
 						Serial.print("LVDS received: ");
 						Serial.println(digitalRead(RECEIVER_PIN));
-					} else if (command.length() == 3 && command[0] == 'T') {
+					} else if (command.length() == 4 && command[0] == 'T') {
 						// should be a space after the T
 						Serial.println("Toggling transmitter pin");
 						digitalWrite(TRANSMITTER_PIN, command[2] == '1');
-					} else if (command == "BP_ON") {
+					} else if (command == "BP_ON\n") {
 						Serial.println("BP = ON");
-						digitalWrite(BLACK_POWDER_PIN_ARDUINO, HIGH);
-					} else if (command == "BP_OFF") {
+						digitalWrite(BLACK_POWDER_PIN_ARDUINO, 1);
+					} else if (command == "BP_OFF\n") {
 						Serial.println("BP = OFF");
-						digitalWrite(BLACK_POWDER_PIN_ARDUINO, LOW);
-					} else if (command == "CONTINUITY") {
+						digitalWrite(BLACK_POWDER_PIN_ARDUINO, 0);
+					} else if (command == "CONTINUITY\n") {
 						Serial.println(black_powder_present() ? "Continuity - black powder present" : "Disconnected - black powder absent");
-					} else if (command == "BEEP") {
+					} else if (command == "BEEP\n") {
 						Serial.print("Beeping... ");
 						Serial.flush();
 
@@ -148,44 +129,56 @@ int main() {
 			case LAUNCH:
 				switch(launch_state) {
 					case PAD:
-						setLEDs(LOW, LOW, LOW);
+						setLEDs(1, 1, 0);
 						for (int i = 0; i < 3; i++) {
+							setLEDs(1, 1, 1);
 							beep();
+							setLEDs(1, 1, 0);
 							delay(1000);
 						}
-						state = FLIGHT;
+						setLEDs(0, 1, 1);
+						for (int i = 0; i < 3; i++) {
+							if (black_powder_present()) beep();
+							else delay(1000);
+						}
+						launch_state = FLIGHT;
 						break;
 					case FLIGHT:
-						setLEDs(HIGH, LOW, LOW); // red LEDs indicate signal never received
+						setLEDs(1, 0, 0); // red LEDs indicate signal never received
 						if (deployment_signal()) launch_state = SIGNAL_RECEIVED;
 						break;
 					case SIGNAL_RECEIVED:
-						setLEDs(LOW, HIGH, LOW); // green LEDs indicate receipt of signal
+						setLEDs(0, 1, 0); // green LEDs indicate receipt of signal
 						// trigger black powder
-						digitalWrite(BLACK_POWDER_PIN_ARDUINO, HIGH);
+						digitalWrite(BLACK_POWDER_PIN_ARDUINO, 1);
 						delay(1000);
-						digitalWrite(BLACK_POWDER_PIN_ARDUINO, LOW);
+						digitalWrite(BLACK_POWDER_PIN_ARDUINO, 0);
 						launch_state = DEPLOYED;
 						break;
 					case DEPLOYED:
-						setLEDs(LOW, LOW, HIGH); // program complete
+						setLEDs(0, 0, 1); // program complete
 						if (black_powder_present()) {
 							beep();
 						}
 						delay(1000);
 						break;
 				}
+				break;
 		}
 		// Read serial and radio, check to enter test mode
 		if (Serial.available()) {
 		  command_available = true;
 		  command = Serial.readString();
+		  Serial.println("Received command: " + command);
+		  if (state == LAUNCH)
+		  	Serial.println("Launch mode.");
+		  else Serial.println("Test mode.");
 		}
 
-		if (command_available && command == "test" && state != TEST_MODE) {
+		if (command_available && command == "test\n" && state != TEST_MODE) {
 		  state = TEST_MODE;
-		  setLEDs(LOW, LOW, LOW);
-		  Serial.println("Ejection entering test mode.");
+		  setLEDs(0, 0, 0);
+		  Serial.println("Deployment entering test mode.");
 		  resetCommand();
 		}
 	}
@@ -214,16 +207,22 @@ char wait_for_signal() {
 }
 
 void beep() {
-	short buzzer = 0;
+	uint8_t buzzer = 0;
 	unsigned long start_time_us = micros();
+	unsigned long last_flip = start_time_us;
+	//digitalWrite(BUZZER_PIN, buzzer);
 	while (micros() - start_time_us < 1000000) {
-		buzzer = (micros() % 1000000 < 500000) ? 0 : 1;
-		digitalWrite(BUZZER_PIN, buzzer);
+		if (micros() - last_flip >= 500) {
+			buzzer = !buzzer;
+			last_flip = micros();
+			digitalWrite(BUZZER_PIN, buzzer);
+			//BUZZER_PORT = (buzzer << BUZZER_PIN);
+		}
 	}
 }
 
 short black_powder_present() {
-	return !(CONTINUITY_PORT & (1 << CONTINUITY_PIN));
+	return !digitalRead(CONTINUITY_PIN);
 }
 
 // Turn on/off 3 LEDs
@@ -235,7 +234,7 @@ void setLEDs(uint8_t red, uint8_t green, uint8_t blue) {
 
 short deployment_signal() {
 	// return !(RECEIVER_PORT & (1 << RECEIVER_PIN));
-	return digitalRead(RECEIVER_PIN) == LOW;
+	return !digitalRead(RECEIVER_PIN);
 }
 
 short launched() {
