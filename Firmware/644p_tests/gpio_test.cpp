40cd2de0bc5692f6db005cb17ecc531f47546b2d
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2018-03-14 20:21:19 -0700

    644p tests

---
+++ Firmware/644p_tests/gpio_test.cpp
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+
+int main() {
+    // DDRA = 0xFF;
+    // PORTA = 0xFF;
+    // PORTA &= ~(1 << PA7);
+
+    DDRB = 0xFF;
+    PORTB = 0x00;
+
+    // DDRC = 0xFF;
+    // PORTC = 0xFF;
+    
+    // DDRD = 0xFF;
+    // PORTD = 0xFF;
+
+    // DDRA = 0xFF;
+    // PORTA = 0x00;
+
+    // DDRB = 0xFF;
+    // PORTB = 0x00;
+
+    // DDRC = 0xFF;
+    // PORTC = 0x00;
+    
+    // DDRD = 0xFF;
+    // PORTD = 0x00;
+    while (true) {
+
+    }
+}
