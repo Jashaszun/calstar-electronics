7c91c828bb7b9b8a5fd98c390c075242e708930b
Nancy <nancyshaw99@gmail.com>
2018-03-17 18:27:22 -0700

    fixed pins

--- Firmware/rover_firmware/ultrasonic_test.cpp
+++ Firmware/rover_firmware/ultrasonic_test.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <NewPing.h>
 
-#define LEFT_SONAR_PING 2
-#define RIGHT_SONAR_PING 4
+#define LEFT_SONAR_TRIG 20
+#define LEFT_SONAR_ECHO 21
+#define RIGHT_SONAR_TRIG 19
+#define RIGHT_SONAR_ECHO 18
 
 int main(void) {
 	init();
 
-	NewPing leftSonar(LEFT_SONAR_PING, LEFT_SONAR_PING, 180);
-    NewPing rightSonar(RIGHT_SONAR_PING, RIGHT_SONAR_PING, 180);
+	NewPing leftSonar(LEFT_SONAR_TRIG, LEFT_SONAR_ECHO, 180);
+    NewPing rightSonar(RIGHT_SONAR_TRIG, RIGHT_SONAR_ECHO, 180);
 
 	Serial.begin(19200);
 	while (1) { 
