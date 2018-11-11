47c3632de697e3bf6d8c21c14c043fbfdfa7efc8
noloerino <jhshi07@gmail.com>
2018-11-10 18:33:30 -0800

    fixed soft deletion; now saves CSV files

--- telemetry_server/server/authorize.sql
+++
@@ -1,5 +0,0 @@
-USE telemetry;
-
--- Replace email with the email address of the user you want to authorize
-UPDATE Users SET authorized=1 WHERE email='rahulkumar@berkeley.edu';
-UPDATE Users SET authorized=1 WHERE email='jhshi@berkeley.edu';
