01d4a3b0821d2f392f3590d0f0486158e8d6c189
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2018-11-19 10:17:01 -0800

    initial ground station firmware

---
+++ ground_systems/firmware/ground_station/main.cpp
@@ -0,0 +1,89 @@
+/*
+ * CalSTAR Avionics Ground Station
+ * 
+ * file: main.cpp
+ * 
+ * Provides communication between laptop and rocket.
+ * Communicates with laptop via UART over USB
+ * Communicates with rocker via radio
+ * Has 2 configurable LEDs and 3 digital inputs on terminal blocks
+ *
+ * LEDs:
+ *  - Rx (PB12)
+ *  - Tx (PB13)
+ * 
+ * Digital Input Ports:
+ *  - Port 1 (PB5)
+ *  - Port 2 (PB6)
+ *  - Port 3 CANNOT USE -- is on BOOT0 instead of a DIO pin
+ *  - Port 4 (PB7)
+ */
+
+#include "mbed.h"
+#include "RFM69/RFM69.h"
+
+#define GATEWAY_ID  (1)
+#define NODE_ID     (11)
+#define NETWORK_ID  (101)
+#define RADIO_INT   (PC_0)
+#define SPI1_MOSI   (PA_7)
+#define SPI1_MISO   (PA_6)
+#define SPI1_SCLK   (PA_5)
+#define SPI1_SSEL   (PA_4)
+#define FREQUENCY   (RF69_433MHZ)
+
+#define IS_RFM69HCW
+
+#define ENCRYPT_KEY ("CALSTARENCRYPTKE") // random 16 bytes that must be the same across all nodes
+
+#define BAUDRATE    (115200)
+
+// These are for if USB fails
+#define UART6_RX    (PC_7)
+#define UART6_TX    (PC_6)
+
+#define PORT1 (PB_5)
+#define PORT2 (PB_6)
+#define PORT4 (PB_7)
+
+#define LED_RX (PB_12)
+#define LED_TX (PB_13)
+
+int main() {
+    DigitalIn  port1(PORT1);
+    DigitalIn  port2(PORT2);
+    DigitalIn  port4(PORT4);
+
+    DigitalOut rx_led(LED_RX);
+    rx_led = 0;
+    DigitalOut tx_led(LED_TX);
+    tx_led = 0;
+
+    Serial pc_usb(USBTX, USBRX);
+    pc_usb.baud(BAUDRATE);
+    pc_usb.set_blocking(false);
+
+    RFM69 radio(SPI1_MOSI, SPI1_MISO, SPI1_SCLK, SPI1_SSEL, RADIO_INT); 
+    radio.initialize(FREQUENCY, NODE_ID, NETWORK_ID);
+    radio.encrypt(ENCRYPT_KEY);
+
+#ifdef IS_RFM69HCW
+    radio.setHighPower();
+#endif
+
+    while (true) {
+        rx_led = 0;
+        tx_led = 0;
+
+        if (radio.receiveDone()) {
+            rx_led = 1;
+            pc_usb.write((const uint8_t *)radio.DATA, radio.DATALEN, NULL); // last arg (NULL) is the tx complete callback
+            if (radio.ACKRequested()) {
+                tx_led = 1;
+                radio.sendACK();
+            }
+        }   
+        
+    }
+    return 0;
+}
