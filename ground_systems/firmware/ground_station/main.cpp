832514fcc9e3649d2a79bfc1b7e3668869a6947f
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-01-16 17:37:17 -0800

    temp

--- ground_systems/firmware/ground_station/main.cpp
+++ ground_systems/firmware/ground_station/main.cpp
@@ -15,7 +15,7 @@
  * Digital Input Ports:
  *  - Port 1 (PB5)
  *  - Port 2 (PB6)
- *  - Port 3 CANNOT USE -- is on BOOT0 instead of a DIO pin
+ *  - Port 3 (PB8) // NOTE: on v3 CANNOT USE -- is on BOOT0 instead of a DIO pin. Can only use on v3.1+
  *  - Port 4 (PB7)
  */
 
@@ -23,20 +23,20 @@
 #include "RFM69/RFM69.h"
 #include <string>
 
-#define GATEWAY_ID  (1)
 #define NODE_ID     (11)
-#define NETWORK_ID  (101)
+#define NETWORK_ID  (100)
 #define RADIO_INT   (PC_0)
 #define SPI1_MOSI   (PA_7)
 #define SPI1_MISO   (PA_6)
 #define SPI1_SCLK   (PA_5)
 #define SPI1_SSEL   (PA_4)
+#define RADIO_RST   (PC_1)
 #define FREQUENCY   (RF69_433MHZ)
-#define TRANSMIT_TO (12)
+#define TRANSMIT_TO (255)
 
 #define IS_RFM69HCW
 
-#define ENCRYPT_KEY ("CALSTARENCRYPTKE") // random 16 bytes that must be the same across all nodes
+#define ENCRYPT_KEY "sampleEncryptKey"//("CALSTARENCRYPTKE") // random 16 bytes that must be the same across all nodes
 
 #define COMMAND_YES_RETRY ("![YES_RETRY]!")
 #define COMMAND_NO_RETRY  ("![NO_RETRY]!")
@@ -49,10 +49,13 @@
 
 #define PORT1 (PB_5)
 #define PORT2 (PB_6)
+#define PORT3 (PB_8)
 #define PORT4 (PB_7)
 
 #define LED_RX (PB_12)
 #define LED_TX (PB_13)
+
+#define LOG(uart, str) uart.write((const uint8_t *)str, strlen(str), NULL);
 
 int main() {
     DigitalIn  port1(PORT1);
@@ -60,15 +63,24 @@ int main() {
     DigitalIn  port4(PORT4);
 
     DigitalOut rx_led(LED_RX);
-    rx_led = 1;
+    rx_led = 0;
     DigitalOut tx_led(LED_TX);
     tx_led = 0;
 
     Serial pc(UART6_TX, UART6_RX);
     pc.baud(BAUDRATE);
-    pc.set_blocking(false);
+    pc.set_blocking(true);
 
     RFM69 radio(SPI1_MOSI, SPI1_MISO, SPI1_SCLK, SPI1_SSEL, RADIO_INT); 
+
+    DigitalOut radio_rst(RADIO_RST);
+    LOG(pc, "![Resetting radio...]!\n");
+    radio_rst = 1;
+    wait(0.5);
+    radio_rst = 0;
+    wait (0.5);
+    LOG(pc, "![Radio reset complete.]!\n");
+
     radio.initialize(FREQUENCY, NODE_ID, NETWORK_ID);
     radio.encrypt(ENCRYPT_KEY);
 
@@ -84,10 +96,10 @@ int main() {
         //pc.write((const uint8_t *) "beginning loop\r\n", sizeof("beginning loop\r\n"), NULL);
 
         if (pc.readable()) {
-            pc.write((const uint8_t *) "pc readable\r\n", sizeof("pc readable\r\n"), NULL);
+            //LOG(pc, "pc readable\n");
             char in = pc.getc();
             if (in == '\n') {
-                pc.write((const uint8_t *) "end line\r\n", sizeof("end line\r\n"), NULL);
+                //LOG(pc, "end line\n");
                 if (line == COMMAND_YES_RETRY) {
                     retry = true;
                     pc.write((const uint8_t *)line.c_str(), line.length(), NULL); 
@@ -95,26 +107,39 @@ int main() {
                     retry = false;
                     pc.write((const uint8_t *)line.c_str(), line.length(), NULL); 
                 } else {
-                    pc.write((const uint8_t *) "send message\r\n", sizeof("send message\r\n"), NULL);
+                    //LOG(pc, "send message\n");
                     if (retry) {
-                        pc.printf("![SENDING W/ RETRY ' %s ']!", line.c_str());
+                        char buf[128];
+                        sprintf(buf, "![SENDING W/ RETRY '%s']!\n", line.c_str());
+                        LOG(pc, buf); 
                         line += '\n';
-                        radio.sendWithRetry(TRANSMIT_TO, line.c_str(), line.length());
+                        //radio.sendWithRetry(TRANSMIT_TO, line.c_str(), line.length());
                     } else {
-                        pc.printf("![SENDING ONCE ' %s ']!", line.c_str());
+                        char buf[128];
+                        sprintf(buf, "![SENDING ONCE ' %s ']!\n", line.c_str());
+                        LOG(pc, buf);
                         line += '\n';
-                        radio.send(TRANSMIT_TO, line.c_str(), line.length());
+                        //radio.send(TRANSMIT_TO, line.c_str(), line.length());
                     } 
                 }
                 line = "";
             } else {
                 line += in;
+                //LOG(pc, "added a char\n");
+                //pc.write((const uint8_t *)line.c_str(), line.length(), NULL);
+                //pc.putc('\n');
+                rx_led = 1;
+                tx_led = 1;
             } 
         }
 
         if (radio.receiveDone()) {
             //rx_led = 1;
+            char buf[128];
+            sprintf(buf, "![RSSI=%d]!", radio.RSSI);
+            LOG(pc, buf);
             pc.write((const uint8_t *)radio.DATA, radio.DATALEN, NULL); // last arg (NULL) is the tx complete callback
+            pc.printf("\n");
             if (radio.ACKRequested()) {
                 //tx_led = 1;
                 radio.sendACK();
--- ground_systems/firmware/tests/echo/main.cpp
+++ ground_systems/firmware/tests/echo/main.cpp
@@ -1,22 +1,19 @@
 #include <mbed.h>
 
-
-#define LED1 (PB_12)
-#define LED2 (PB_13)
-
-#define UART6_RX    (PC_7)
-#define UART6_TX    (PC_6)
+#define UART6_RX    (PA_3)
+#define UART6_TX    (PA_2)
 
 int main() {
     Serial pc(UART6_TX, UART6_RX);
     pc.baud(9600);
 
-    DigitalOut led1(LED1);
-    led1 = 0;
-    DigitalOut led2(LED2);
-    led2 = 0;
+    DigitalOut dio1(PB_15);
+    DigitalOut dio2(PB_13);
+    dio1=1;
+    dio2=0;
 
     while (true) {
+        pc.putc('h');
         if (pc.readable())
             pc.putc(pc.getc());
     }
