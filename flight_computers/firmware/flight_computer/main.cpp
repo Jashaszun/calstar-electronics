4d346313cd84184d5454b5836b50003613dc6cc8
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2019-01-26 17:32:57 -0800

    Altimeter and IMU tests (untested)

--- flight_computers/firmware/flight_computer/main.cpp
+++ flight_computers/firmware/flight_computer/main.cpp
@@ -15,9 +15,16 @@
 #define DEBUG_RX PA_3
 #define DEBUG_TX PA_2
 
+#define BAUDRATE 9600
+
 #define STATE_LED_RED PA_8
 #define STATE_LED_GREEN PC_9
 #define STATE_LED_BLUE PC_8
+
+// For black box --> testing
+//#define STATE_LED_RED PC_8
+//#define STATE_LED_GREEN PA_8
+//#define STATE_LED_BLUE PC_9
 
 #define BP1_IGNITE PB_15
 #define BP1_CONT   PB_14
@@ -53,9 +60,19 @@ DigitalOut led_red(STATE_LED_RED);
 DigitalOut led_green(STATE_LED_GREEN);
 DigitalOut led_blue(STATE_LED_BLUE);
 
+Altitude altitude;
 MPL3115A2 altimeter(&i2c_sensors);
 
-int bp_test() {
+MPU9250 imu;
+
+void led_color(bool r, bool g, bool b) {
+    // Flip because common anode
+    led_red.write(!r);
+    led_green.write(!g);
+    led_blue.write(!b);
+}
+
+void bp_test() {
 
     DigitalOut bp7_ig(BP7_IGNITE);
     bp7_ig = 0;
@@ -66,18 +83,14 @@ int bp_test() {
         bp7_ig = 0;
         wait(0.5);
     }
-
-    return 0;
 }
 
-int rainbow_test() {
+void rainbow_test() {
     while (true) {
         for (int r = 0; r < 2; r++) {
             for (int g = 0; g < 2; g++) {
                 for (int b = 0; b < 2; b++) {
-                    led_red.write(r);
-                    led_green.write(g);
-                    led_blue.write(b);
+                    led_color(r, g, b);
                     wait(1);
                 }
             }
@@ -85,11 +98,42 @@ int rainbow_test() {
     }
 }
 
-int altimeter_test() {
+void altimeter_test() {
+    led_color(1, 0, 0);
+    wait(10);
+    led_color(0, 0, 1);
     altimeter.init();
+    debug_talk.baud(BAUDRATE);
+    debug_talk.printf("Begin program\n\n");
+    uint8_t whoami = altimeter.whoAmI();
+    if (whoami == 0xC4) {
+        debug_talk.printf("Successfully connected\n");
+        while (true) {
+            altimeter.readAltitude(&altitude);
+            debug_talk.printf("Altitude: %f\n", altitude.altitude());
+            wait(0.5);
+        }
+    } else {
+        debug_talk.printf("Failed to connect to altimeter\n");
+    }
+}
 
+void imu_test() {
+    // Could just use:
+    // https://os.mbed.com/users/onehorse/code/MPU9250AHRS/file/4e59a37182df/main.cpp/
+    led_color(0, 1, 0);
+    wait(10);
+    led_color(0, 0, 1);
+    debug_talk.baud(BAUDRATE);
+    debug_talk.printf("Begin program\n\n");
+    uint8_t whoami = imu.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+    if (whoami == 0x71) {
+        debug_talk.printf("Successfully connected\n");
+    } else {
+        debug_talk.printf("Failed to connect to IMU\n");
+    }
 }
 
 int main() {
-    rainbow_test();
+    altimeter_test();
 }
