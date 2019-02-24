3a5a3388ed8410c2ffcbce7ce5b337feb929084e
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-02-24 12:13:25 -0800

    now includes io3 (previously on boot0), fixed LOG to printf

--- ground_systems/ground_station/main.cpp
+++ ground_systems/ground_station/main.cpp
@@ -4,7 +4,7 @@
  * file: main.cpp
  * 
  * Provides communication between laptop and rocket.
- * Communicates with laptop via UART over USB
+ * Communicates with laptop via UART over USB or just UART
  * Communicates with rocker via radio
  * Has 2 configurable LEDs and 3 digital inputs on terminal blocks
  *
@@ -15,7 +15,7 @@
  * Digital Input Ports:
  *  - Port 1 (PB5)
  *  - Port 2 (PB6)
- *  - Port 3 (PB8) // NOTE: on v3 CANNOT USE -- is on BOOT0 instead of a DIO pin. Can only use on v3.1+
+ *  - Port 3 (PB8) // NOTE: on v3.0 CANNOT USE -- is on BOOT0 instead of a DIO pin. Can only use on v3.1+
  *  - Port 4 (PB7)
  */
 
@@ -47,20 +47,19 @@
 #define UART6_RX    (PC_7)
 #define UART6_TX    (PC_6)
 
-#define PORT1 (PB_5)
-#define PORT2 (PB_6)
-#define PORT3 (PB_8)
-#define PORT4 (PB_7)
+#define IO1 (PB_5)
+#define IO2 (PB_6)
+#define IO3 (PB_8)
+#define IO4 (PB_7)
 
 #define LED_RX (PB_12)
 #define LED_TX (PB_13)
 
-#define LOG(uart, str) uart.write((const uint8_t *)str, strlen(str), NULL);
-
 int main() {
-    DigitalIn  port1(PORT1);
-    DigitalIn  port2(PORT2);
-    DigitalIn  port4(PORT4);
+    DigitalIn  io1(IO1);
+    DigitalIn  io2(IO2);
+    DigitalIn  io3(IO3);
+    DigitalIn  io4(IO4);
 
     DigitalOut rx_led(LED_RX);
     rx_led = 0;
@@ -69,17 +68,16 @@ int main() {
 
     Serial pc(UART6_TX, UART6_RX);
     pc.baud(BAUDRATE);
-    pc.set_blocking(true);
 
     RFM69 radio(SPI1_MOSI, SPI1_MISO, SPI1_SCLK, SPI1_SSEL, RADIO_INT); 
 
     DigitalOut radio_rst(RADIO_RST);
-    LOG(pc, "![Resetting radio...]!\n");
+    pc.printf("![Resetting radio...]!\n")
     radio_rst = 1;
     wait(0.5);
     radio_rst = 0;
     wait (0.5);
-    LOG(pc, "![Radio reset complete.]!\n");
+    pc.printf("![Radio reset complete.]!\n");
 
     radio.initialize(FREQUENCY, NODE_ID, NETWORK_ID);
     radio.encrypt(ENCRYPT_KEY);
@@ -91,15 +89,12 @@ int main() {
     std::string line = "";
     bool retry = false;
     while (true) {
-        //rx_led = 0;
-        //tx_led = 0;
-        //pc.write((const uint8_t *) "beginning loop\r\n", sizeof("beginning loop\r\n"), NULL);
+        rx_led = 0;
+        tx_led = 0;
 
         if (pc.readable()) {
-            //LOG(pc, "pc readable\n");
             char in = pc.getc();
             if (in == '\n') {
-                //LOG(pc, "end line\n");
                 if (line == COMMAND_YES_RETRY) {
                     retry = true;
                     pc.write((const uint8_t *)line.c_str(), line.length(), NULL); 
@@ -107,41 +102,31 @@ int main() {
                     retry = false;
                     pc.write((const uint8_t *)line.c_str(), line.length(), NULL); 
                 } else {
-                    //LOG(pc, "send message\n");
                     if (retry) {
-                        char buf[128];
-                        sprintf(buf, "![SENDING W/ RETRY '%s']!\n", line.c_str());
-                        LOG(pc, buf); 
+                        pc.printf("![SENDING W/ RETRY '%s']!\n", line.c_str());
                         line += '\n';
-                        //radio.sendWithRetry(TRANSMIT_TO, line.c_str(), line.length());
+                        radio.sendWithRetry(TRANSMIT_TO, line.c_str(), line.length());
                     } else {
-                        char buf[128];
-                        sprintf(buf, "![SENDING ONCE ' %s ']!\n", line.c_str());
-                        LOG(pc, buf);
+                        pc.printf("![SENDING ONCE ' %s ']!\n", line.c_str());
                         line += '\n';
-                        //radio.send(TRANSMIT_TO, line.c_str(), line.length());
+                        radio.send(TRANSMIT_TO, line.c_str(), line.length());
                     } 
                 }
                 line = "";
             } else {
                 line += in;
-                //LOG(pc, "added a char\n");
-                //pc.write((const uint8_t *)line.c_str(), line.length(), NULL);
-                //pc.putc('\n');
                 rx_led = 1;
                 tx_led = 1;
             } 
         }
 
         if (radio.receiveDone()) {
-            //rx_led = 1;
-            char buf[128];
-            sprintf(buf, "![RSSI=%d]!", radio.RSSI);
-            LOG(pc, buf);
+            rx_led = 1;
+            pc.printf("![RSSI=%d]!", radio.RSSI);
             pc.write((const uint8_t *)radio.DATA, radio.DATALEN, NULL); // last arg (NULL) is the tx complete callback
             pc.printf("\n");
             if (radio.ACKRequested()) {
-                //tx_led = 1;
+                tx_led = 1;
                 radio.sendACK();
             }
         }   
