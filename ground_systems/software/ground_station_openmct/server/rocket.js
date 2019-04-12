bf5f86f9003e83d4202fba061c590bfce744ae2a
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-04-12 01:43:34 -0700

    merge resolve

--- ground_systems/software/ground_station_openmct/server/rocket.js
+++ ground_systems/software/ground_station_openmct/server/rocket.js
@@ -4,42 +4,59 @@ const rl = require('readline');
 
 function Rocket(comPort) {
     this.state = {
+        "fc.pwr": 0,
         "fc.state": 0,
-        "accel.x": 0,
-        "accel.y": 0,
-        "accel.z": 1,
-        "alt": 0,
+        "fc.alt": 0,
+        "tpc.gps": "",
         "transmission": "",
         "comms.recd": 0,
-        "comms.sent": 0
+        "comms.sent": 0,
+        "gs.rssi": 0,
+        "tpc.bat_v": "",
+        "tpc.state": ""
     };
+    this.total_comms_recd = 0;
     this.history = {};
     this.listeners = [];
     Object.keys(this.state).forEach(function (k) {
         this.history[k] = [];
     }, this);
 
-    setInterval(() => {
-        this.generateTelemetry();
-    }, 500);
+    // setInterval(() => {
+    //     this.generateTelemetry();
+    // }, 500);
 
-    const port = new SerialPort(comPort, {baudRate: 115200});
+    const port = new SerialPort('/dev/ttyS13', { baudRate: 115200 });
     const parser = port.pipe(new Readline());
 
     parser.on('data', (line) => {
-        const words = String(line).split(' ');
-        if (words.length == 2) {
-            this.state[words[0]] = String(words[1]);
-            this.generateTelemetry();
+        // console.log(line);
+        try {
+            // console.log(JSON.parse("{\"timestamp\": 1, \"id\": \"gs.rssi\", \"value\": -30}"));
+            obj = JSON.parse(line);
+            obj["timestamp"] = Date.now();
+            // obj["timestamp"] = Math.round(obj["timestamp"] / 1000000);
+            if (obj["id"] === "comms.recd") {
+                this.total_comms_recd += obj["value"];
+                obj["value"] = this.total_comms_recd;
+            }
+            console.log(obj);
+            this.notify(obj);
+            this.history[obj["id"]].push(obj);
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                console.log("syntax error");
+            }
+            console.log("error");
         }
 
         this.updateConsole(line);
     });
 
     process.stdin.on('data', (line) => {
-        const words = String(line).split(' ');
-        this.state[words[0]] = Number(words[1]); 
-        this.generateTelemetry();
+        // const words = String(line).split(' ');
+        // this.state[words[0]] = Number(words[1]);
+        // this.generateTelemetry();
     });
 
     // Clear console
@@ -53,8 +70,9 @@ function Rocket(comPort) {
 Rocket.prototype.generateTelemetry = function () {
     var timestamp = Date.now(), sent = 0;
     Object.keys(this.state).forEach(function (id) {
-        var state = { timestamp: timestamp, value: this.state[id], id: id};
+        var state = { timestamp: timestamp, value: this.state[id], id: id };
         this.notify(state);
+        console.log(state);
         this.history[id].push(state);
         this.state["comms.sent"] += JSON.stringify(state).length;
         this.state["transmission"] += ", " + id + ": " + String(this.state[id]);
@@ -63,7 +81,7 @@ Rocket.prototype.generateTelemetry = function () {
 
 };
 
-Rocket.prototype.updateConsole = function(line) {
+Rocket.prototype.updateConsole = function (line) {
     // TODO: Make this look nice and separate data cleanly
     rl.cursorTo(process.stdout, 0, 20);
     rl.clearLine(process.stdout);
@@ -85,6 +103,6 @@ Rocket.prototype.listen = function (listener) {
     };
 };
 
-module.exports = function(comPort) {
+module.exports = function (comPort) {
     return new Rocket(comPort)
 };
